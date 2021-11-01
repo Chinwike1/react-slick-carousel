7958cb2d87f2dac19744a4b95f16a6420431a635
Chinwike <maduabuchichizzy@gmail.com>
2021-11-01 09:50:43 +0100

    made carousel responsive

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -13,6 +13,20 @@ export default function Carousel() {
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   }
 
   const hotelCards = [
